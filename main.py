77496acd272a003679d368e804c55a9137c8273c
Caio Pedro <caiopedro28@gmail.com>
2025-12-03 10:43:46 -0300

    função de sair

--- main.py
+++ main.py
@@ -5,21 +5,31 @@ def rolar(n_dados, faces):
     return [random.randint(1, faces) for _ in range(n_dados)]
 
 def main():
-    entrada = input("Digite a rolagem (ex: 3d20): ").lower()
+    print("Digite rolagens no formato XdY (ex: 3d20, 2d6).")
+    print("Digite 'sair' para encerrar.\n")
 
-    # divide na letra 'd'
-    try:
-        n_dados, faces = entrada.split("d")
-        n_dados = int(n_dados)
-        faces = int(faces)
-    except:
-        print("Formato inválido! Use algo como 3d20, 2d6, 1d100...")
-        return
+    while True:
+        entrada = input("Rolagem: ").lower().strip()
 
-    resultado = rolar(n_dados, faces)
-    print(f"Rolando {n_dados}d{faces}...")
-    time.sleep(0.5)
-    print(f"Resultados: {resultado} -> soma = {sum(resultado)}")
+        # saida do programa
+        if entrada == "sair":
+            print("Encerrando...")
+            break  # sai do while
+
+        # tenta pegar XdY
+        try:
+            n_dados, faces = entrada.split("d")
+            n_dados = int(n_dados)
+            faces = int(faces)
+        except:
+            print("Formato inválido! Use algo como 3d20 ou digite 'sair'.\n")
+            continue  # volta ao inicio
+
+        # rolagem
+        resultado = rolar(n_dados, faces)
+        print(f"Rolando {n_dados}d{faces}...")
+        time.sleep(0.5)
+        print(f"Resultado: {resultado} -> soma = {sum(resultado)}\n")
 
 if __name__ == "__main__":
     main()

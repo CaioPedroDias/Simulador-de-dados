02ae3a08bc50d4ae5dc6416b37d1c36bf8640b0a
Caio Pedro <caiopedro28@gmail.com>
2025-12-03 09:55:29 -0300

    mudança de formato de leitura

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import random
+import time
 
 def rolar(n_dados, faces):
     return [random.randint(1, faces) for _ in range(n_dados)]
 
 def main():
-    try:
-        n_dados = int(input("Quantos dados? : "))
-        faces = int(input("Quantas faces? : "))
-        vezes = int(input("Quantas vezes quer rolar? : "))
+    entrada = input("Digite a rolagem (ex: 3d20): ").lower()
 
-    except ValueError:
-        print("Entrada inválida. Use números inteiros.")
+    # divide na letra 'd'
+    try:
+        n_dados, faces = entrada.split("d")
+        n_dados = int(n_dados)
+        faces = int(faces)
+    except:
+        print("Formato inválido! Use algo como 3d20, 2d6, 1d100...")
         return
 
-    for i in range(vezes):
-        resultado = rolar(n_dados, faces)
-        print(f"Rolada {i+1}: {resultado} -> soma = {sum(resultado)}")
+    resultado = rolar(n_dados, faces)
+    print(f"Rolando {n_dados}d{faces}...")
+    time.sleep(0.5)
+    print(f"Resultados: {resultado} -> soma = {sum(resultado)}")
 
 if __name__ == "__main__":
     main()
